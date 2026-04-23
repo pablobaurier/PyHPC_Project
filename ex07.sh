a75314f3015f3faeaec0306fd3d1b5d0b18aced0
pablobaurier <pablo.bauga@gmail.com>
2026-04-23 15:58:53 +0200

    added ex7 data

---
+++ ex07.sh
@@ -0,0 +1,26 @@
+#!/bin/sh 
+
+#BSUB -q hpc
+#BSUB -J ex07_g101
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -R "select[model==XeonGold6226R]"
+#BSUB -W 00:59
+#BSUB -N 
+#BSUB -o out/Output_%J.out 
+#BSUB -e err/Output_%J.err 
+
+
+# Initialize Python environment 
+source /dtu/projects/02613_2025/conda/conda_init.sh 
+conda activate 02613_2026
+
+
+# Run Python script 
+python simulate_PB.py 50
+
+python simulate_PB_2.py 50
+
+
+
---
+++ ex07_4.sh
@@ -0,0 +1,27 @@
+#!/bin/sh 
+
+#BSUB -q hpc
+#BSUB -J ex07_g101
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "select[model==XeonGold6226R]"
+#BSUB -W 01:30
+#BSUB -N 
+#BSUB -o out/Output_%J.out 
+#BSUB -e err/Output_%J.err 
+
+
+# Initialize Python environment 
+source /dtu/projects/02613_2025/conda/conda_init.sh 
+conda activate 02613_2026
+
+
+# Run Python script 
+# python simulate_PB.py 50
+
+python simulate_PB_4.py 1
+python simulate_PB_4.py 2
+python simulate_PB_4.py 3
+python simulate_PB_4.py 4
+
